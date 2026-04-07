d9f7f27380071fcce7484692c89004eb2fcbf8b8
tolyabui300899 <tinbt@nganluong.vn>
2026-04-07 11:13:00 +0700

    test

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,7 +5,7 @@
       <div class="sidebar-header">
         <div class="logo">
           <Heart class="logo-icon" />
-          <span v-if="!sidebarCollapsed" class="logo-text">HealthCare</span>
+          <span v-if="!sidebarCollapsed" class="logo-text">HealthCare demo</span>
         </div>
         <button v-if="!sidebarCollapsed" @click="toggleSidebar" class="sidebar-toggle">
           <Menu />
